9a0e9cd07ee8648387f41798a013c2d3225ee0a7
MythsList <mythslistofficial@gmail.com>
2022-08-05 00:38:16 +0200

    Hope this works lol

--- 404.js
+++
@@ -1,9 +0,0 @@
-const errors = ["404", "134"];
-const randomnum = Math.floor(Math.random() * 26) + 1;
-
-if (randomnum >= 20) {
-    document.querySelectorAll(':root').style.setProperty("--error", errors[1]);
-}  
-else {
-    document.querySelectorAll(':root').style.setProperty("--error", errors[0]);
-}
