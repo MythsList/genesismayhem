3e4af3306a45947b08cf16c930ea35291cd7e2ee
MythsList <mythslistofficial@gmail.com>
2022-08-05 00:23:00 +0200

    another test

--- 404.js
+++ 404.js
@@ -1,9 +1,9 @@
-var errors = ["404", "134"];
-var randomnum = Math.floor(Math.random() * 26) + 1;
+const errors = ["404", "134"];
+const randomnum = Math.floor(Math.random() * 26) + 1;
 
 if (randomnum >= 20) {
-    document.documentElement.style.setProperty("--error", errors[1]);
+    document.querySelectorAll(':root').style.setProperty("--error", errors[1]);
 }  
 else {
-    document.documentElement.style.setProperty("--error", errors[0]);
+    document.querySelectorAll(':root').style.setProperty("--error", errors[0]);
 }
