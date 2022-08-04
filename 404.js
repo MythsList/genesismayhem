72712cbcee63093ea971605dd9d3b5ca0ca0a77a
MythsList <mythslistofficial@gmail.com>
2022-08-04 23:28:14 +0200

    more tests

--- 404.js
+++ 404.js
@@ -1,7 +1,7 @@
 var errors = ["404", "134"];
 var randomnum = Math.floor(Math.random() * 26) + 1;
 
-if (randomnum == 1) {
+if (randomnum >= 20) {
     document.documentElement.style.setProperty("--error", errors[1]);
 }  
 else {
