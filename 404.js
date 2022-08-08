17a0fa9f32331987fa1dda8add38b6cca905d2ca
MythsList <mythslistofficial@gmail.com>
2022-08-08 23:59:50 +0200

    better

--- 404.js
+++ 404.js
@@ -1,15 +1,9 @@
 const randomnum = Math.floor(Math.random() * 26) + 1;
   
 if (randomnum >= 25 || randomnum <= 1) {
-    if (typeof(Storage) !== "undefined") {
-        localStorage.amountofnoway = Number(localStorage.amountofnoway) + 1;
-    }
-
     document.title = "Genesis Mayhem | NO WAY!";
     document.getElementById("errormsg").innerHTML = "Error 0134";
     document.getElementById("noway").innerHTML = "No way? No way!";
     document.getElementById("audio").controls = "true";
-    document.getElementById("counter").innerHTML = localStorage.amountofnoway;
-    document.getElementById("counter").style.display = "block";
-    document.getElementById("copyright").style.marginTop = "-1em";
+    document.getElementById("copyright").style.marginTop = "0.5em";
 }  
