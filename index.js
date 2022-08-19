95d83c5cbe549f3248b3eb28892774f1f9fc4bdd
MythsList <mythslistofficial@gmail.com>
2022-08-20 01:56:30 +0200

    glow up index

---
+++ index.js
@@ -0,0 +1,11 @@
+document.querySelector("#gamebanana").onclick = () => {
+    location.href = "404.html"
+}
+
+document.querySelector("#credits").onclick = () => {
+    location.href = "404.html"
+}
+
+document.querySelector("#soundtest").onclick = () => {
+    location.href = "404.html"
+}
